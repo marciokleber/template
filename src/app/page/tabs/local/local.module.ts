680d09f92383ea34a79fd2ee730799a0f8ab0d77
Marcio Kleber Junior Lopes de Souza <msouza@tre-pa.jus.br>
2024-09-16 17:39:30 -0300

    refactor :Ajuste na template local e criação da tab scanner

--- src/app/page/tabs/local/local.module.ts
+++ src/app/page/tabs/local/local.module.ts
@@ -3,18 +3,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {LocalPage} from './local.page';
-import {ExploreContainerComponentModule} from '../explore-container/explore-container.module';
-
-import {LocalPageRoutingModule} from './local-routing.module';
-import {StandardListComponent} from "../@core/components/standard-list/standard-list.component";
-import {StandardSearchbarComponent} from "../@core/components/standard-searchbar/standard-searchbar.component";
+import {LocalPageRoutingModule} from "./local-routing.module";
+import {StandardListComponent} from "../../../@core/components/standard-list/standard-list.component";
+import {StandardSearchbarComponent} from "../../../@core/components/standard-searchbar/standard-searchbar.component";
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     LocalPageRoutingModule,
     StandardListComponent,
     StandardSearchbarComponent
