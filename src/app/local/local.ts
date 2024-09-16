08135d0c442fd1cde01ba9f15abe2a9edc86bfb3
jcruz <jcruz@tre-pa.jus.br>
2024-09-16 14:54:17 -0300

    Implementaçao inicial da arquitetura

---
+++ src/app/local/local.ts
@@ -0,0 +1,5 @@
+export class Local {
+  id?: number;
+  idLocal?: string;
+  nome?: string;
+}
--- src/app/service/locals.service.ts
+++ src/app/service/locals.service.ts
@@ -1,36 +1,35 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs';
-import {StorageService} from "./storage.service";
-import { Http } from '@capacitor-community/http';
-
+import {StandardNgService} from "../@core/standard-ng-service";
+import {environment} from "../../environments/environment";
+import {Local} from "../local/local";
 
 
 @Injectable({
   providedIn: 'root',
 })
-export class LocalsService {
+export class LocalsService extends StandardNgService<Local, number>{
 
-  URL_API: string = `http://localhost:8080/app-ponto/api/locals`;
+  // URL_API: string = `http://localhost:8080/app-ponto/api/locals`;
+  protected API_PATH: string = `${environment.contextPath}/api/locals`;
 
-  protected constructor(protected http: HttpClient, private storageService: StorageService) {
-  }
+  // protected constructor(protected http: HttpClient, private storageService: StorageService) {
+  // }
 
-  async findAll(q: string, page: number, size: number, summaryOptions: string): Promise<Observable<any>> {
-    const token = await this.storageService.find('access_token');
-
-    let headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-
-    let params = new HttpParams()
-      //.set('q', q) // seu parâmetro de query RSQLParam
-      //.set('page', page)
-      //.set('size', size)
-      //.set('summaryOptions', summaryOptions);
-
-    return this.http.get<any>(`${this.URL_API}`, {params});
-  }
+  // async findAll(q: string, page: number, size: number, summaryOptions: string): Promise<Observable<any>> {
+  //   const token = await this.storageService.find('access_token');
+  //
+  //   let headers = new HttpHeaders({
+  //     'Authorization': `Bearer ${token}`
+  //   });
+  //
+  //   let params = new HttpParams()
+  //     //.set('q', q) // seu parâmetro de query RSQLParam
+  //     //.set('page', page)
+  //     //.set('size', size)
+  //     //.set('summaryOptions', summaryOptions);
+  //
+  //   return this.http.get<any>(`${this.URL_API}`, {params});
+  // }
 
   // async findAll(): Promise<any> {
   //   const token = await this.storageService.find('access_token');
